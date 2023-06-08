8ad982afb6393d51e491336b744311528e07f232
Manjot <manjotsingh1901@gmail.com>
2023-06-08 14:21:25 +0530

    Added CSS variables and made general structure of the sign up page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const authRoutes = require("./routes/auth-routes");
+const path = require("path");
+
 const app = express();
+
+// setting up 'ejs'
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
