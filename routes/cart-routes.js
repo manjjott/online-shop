9d3f0ba277edbc382db6d5002fd2447769884887
Manjot <manjotsingh1901@gmail.com>
2023-06-12 12:55:10 +0530

    Patch requests for cart update in cart page

--- routes/cart-routes.js
+++ routes/cart-routes.js
@@ -3,8 +3,10 @@ const cartController = require("../controllers/cart-controller");
 
 const router = express.Router();
 
-router.post("/items",cartController.addCartItem);
+router.post("/items", cartController.addCartItem);
 
+router.get("/", cartController.getCart);
 
+router.patch("/items", cartController.updateCartItem);
 
 module.exports = router;
